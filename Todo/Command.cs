c2198ab18adccdd76c425177103792df60e0f7e4
GET Academy <terje@getacademy.no>
2025-01-07 18:02:44 +0100

    Update Command.cs

--- Todo/Command.cs
+++ Todo/Command.cs
@@ -18,13 +18,13 @@
         {
         }
 
-        private static string Ask(string question)
+        protected static string Ask(string question)
         {
             Console.Write(question);
             return Console.ReadLine();
         }
 
-        private static int AskForInt(string question)
+        protected static int AskForInt(string question)
         {
             var answer = Ask(question);
             return Convert.ToInt32(answer);
