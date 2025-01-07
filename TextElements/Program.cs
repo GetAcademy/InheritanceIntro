db1c31a53a7d7944e957b3afd26c376b2032f827
GET Academy <terje@getacademy.no>
2025-01-07 09:15:02 +0100

    Initial commit

---
+++ TextElements/Program.cs
@@ -0,0 +1,61 @@
+﻿using TextElements.SimpleElements;
+
+var centeredHeader = new CenteredHeader("Hei", '*');
+var bulletPointA = new BulletPoint("a");
+var bulletPointB = new BulletPoint("b");
+var bulletPointC = new BulletPoint("c");
+var text = string.Concat(Enumerable.Repeat("Det var en gang.", 22));
+var indentedParagraph = new IndentedParagraph(text);
+var invertedText = new InvertedText("Terje");
+
+var textElements = new ITextElement[]
+{
+    centeredHeader, bulletPointA, bulletPointB, bulletPointC, indentedParagraph, invertedText
+};
+
+foreach (var textElement in textElements)
+{
+    textElement.Show();
+}
+
+/*
+v1 - uten interface
+
+foreach (var textElement in textElements)
+{
+    if (textElement is CenteredHeader)
+    {
+        var element = (CenteredHeader)textElement;
+        element.Show();
+    }
+    else if (textElement is BulletPoint)
+    {
+        var element = (BulletPoint)textElement;
+        element.Show();
+    }
+}
+*/
+
+/*
+Utgangspunkt
+
+   var centeredHeader = new CenteredHeader("Hei", '*');
+   var bulletPointA = new BulletPoint("a");
+   var bulletPointB = new BulletPoint("b");
+   var bulletPointC = new BulletPoint("c");
+   var text = string.Concat(Enumerable.Repeat("Det var en gang.", 22));
+   var indentedParagraph = new IndentedParagraph(text);
+   var invertedText = new InvertedText("Terje");
+   
+   
+   bulletPointA.Show();
+   bulletPointB.Show();
+   bulletPointC.Show();
+   centeredHeader.Show();
+   indentedParagraph.Show();
+   bulletPointA.Show();
+   bulletPointB.Show();
+   bulletPointC.Show();
+   invertedText.Show();
+   
+ */
---
+++ TextElements/SimpleElements/IndentedParagraph.cs
@@ -0,0 +1,18 @@
+﻿namespace TextElements.SimpleElements
+{
+    internal class IndentedParagraph : ITextElement
+    {
+        private string _text;
+
+        public IndentedParagraph(string text)
+        {
+            _text = text;
+        }
+
+        public void Show()
+        {
+            Console.CursorLeft = 4;
+            Console.WriteLine(_text);
+        }
+    }
+}
